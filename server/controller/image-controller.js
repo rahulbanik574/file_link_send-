de05dfc3e437b87ab03d523725e92540129fa23e
rahulbanik574 <rahulbanik574@gmial.com>
2024-01-31 11:00:29 +0530

    Add initials file

---
+++ server/controller/image-controller.js
@@ -0,0 +1,41 @@
+
+
+ import File from '../models/file.js';
+// import bcrypt from 'bcrypt';
+// import dotenv from 'dotenv';
+
+// dotenv.config();
+
+ export const uploadImage = async (request, response) => {
+    const fileObj = {
+        path: request.file.path,
+        name: request.file.originalname,
+
+//return respons.status(200).json({msg:'hello'})
+     }
+    
+     try {
+         const file = await File.create(fileObj);
+         response.status(200).json({ path: `http://localhost:${process.env.PORT}/file/${file._id}`});
+     } catch (error) {
+        console.error(error.message);
+         response.status(500).json({ error: error.message });
+    }
+ }
+
+export const downloadImage = async (request, response) => {
+     try {   
+         const file = await File.findById(request.params.fileId);
+        
+         file.downloadCount++;
+
+         await file.save();
+
+         response.download(file.path, file.name);
+     } catch (error) {
+         console.error(error.message);
+         response.status(500).json({ msg: error.message });
+     }
+ }
+
+ 
